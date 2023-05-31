02607413244db572ce96f4524b205f7e568a2503
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 14:53:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+  var len = str.length;
+    var mid = Math.floor(len/2);
 
+    for ( var i = 0; i < mid; i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 module.exports = palindrome
