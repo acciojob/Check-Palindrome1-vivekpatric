acae691dd6c86bf88aeacdc1afa2c20a2d19b231
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 14:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-  var len = str.length;
+	let result = str.toLowerCase();
+
+  var len = result.length;
     var mid = Math.floor(len/2);
 
     for ( var i = 0; i < mid; i++ ) {
-        if (str[i] !== str[len - 1 - i]) {
+        if (result[i] !== result[len - 1 - i]) {
             return false;
         }
     }
